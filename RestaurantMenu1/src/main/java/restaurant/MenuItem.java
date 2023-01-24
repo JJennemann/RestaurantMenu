c79a52f9460e91603a624e723289517e16527a4e
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-01-24 10:27:15 -0600

    completed studio 4.8 created initial menu and menuitems classes

---
+++ RestaurantMenu1/src/main/java/restaurant/MenuItem.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+
+import java.util.Date;
+
+public class MenuItem {
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private Date dateAdded;
+
+    public MenuItem(String name, String description, double price, String category){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.dateAdded = new Date();
+    }
+}
