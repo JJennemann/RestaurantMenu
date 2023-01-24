40528ac5dd1b7b0996975d77d839d48b3c86b5d6
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-01-24 10:46:32 -0600

    Having trouble figuring out branches. Need to review, but to not get bogged down with that now and just completing everything on main branch.

---
+++ RestaurantMenu1/src/main/java/restaurant/Restaurant.java
@@ -0,0 +1,29 @@
+package restaurant;
+
+public class Restaurant {
+    public static void main(String[] args){
+
+        MenuItem entreeOne = new MenuItem("Meatloaf", "Meatloaf better than your mommas!",
+                15.99, "Entree");
+
+        MenuItem entreeTwo = new MenuItem("Spaghetti and Meatballs", "Spaghetti made" +
+                "with our famous meatballs!", 16.99, "Entree");
+
+        MenuItem entreeThree = new MenuItem("Country Fried Steak", "Steak fried straight" +
+                "from the country!", 12.99, "Entree");
+
+        MenuItem appOne = new MenuItem("Jalapeno Poppers", "You'll never pop another popper" +
+                "in your mouth after our jalapeno poppers!", 7.99, "Appetizer");
+
+        MenuItem appTwo = new MenuItem("Fried Pickles", "You like pickles? Try em' fried!",
+                5.99, "Appetizer");
+
+        MenuItem dessertOne = new MenuItem("Tiramisu", "It's our only dessert", 9.99,
+                "Dessert");
+
+        Menu currentMenu = new Menu();
+
+        currentMenu.addMenuItem(entreeOne);
+        System.out.println(currentMenu);
+    }
+}
